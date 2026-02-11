c487ec3b8418eb90fe34fe6fcc5a0f7f430d5b4d
KingNovaxS <modlebrun@gmail.com>
2026-02-11 10:37:21 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc'
 
-// Refer to this main Documentation - https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SeMiaParaSiempre/', 
 })
